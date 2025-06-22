97f9b5c2be4463803d261460fa57ed60e0cbb07d
Aarushthegamemaster <aarushchandra0604@gmail.com>
2025-06-22 12:29:46 -0700

    Homework_For_Python

---
+++ Homework.py
@@ -0,0 +1,6 @@
+def pi_function(P):
+    return 3.14*P
+
+radius = int(input("Please enter the radius of the circle(It has to be whole number):"))
+
+print("The circumfrence of the radius",radius, "is",pi_function(radius))
